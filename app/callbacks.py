b860a60a6b908dc2a11b77794123b81832ae5f00
Fraxix <r.super@gmx.net>
2026-01-11 21:26:56 +0100

    v0.4.2 – Improve password security using secrets instead of random, code cleanups

--- app/callbacks.py
+++ app/callbacks.py
@@ -1,37 +1,34 @@
 import dearpygui.dearpygui as dpg
 from app.config import ERROR_NO_CHAR_SELECTED,INFO_PASSWORD_COPIED,ERROR_PASSWORD_EMPTY
-import random
+import secrets
 import string
 
-def generate_password(sender):
-
-    if not dpg.get_value('auto_generate_toggle') and sender != 'generate_button':
-        return
-
-    include_uppercase = dpg.get_value('include_uppercase')
-    include_lowercase = dpg.get_value('include_lowercase')
-    include_numbers   = dpg.get_value('include_numbers')
-    include_symbols   = dpg.get_value('include_symbols')
-
+def get_characters():
     characters = ""
-    if include_uppercase:
+    if dpg.get_value('include_uppercase'):
         characters += string.ascii_uppercase
-    if include_lowercase:
+    if dpg.get_value('include_lowercase'):
         characters += string.ascii_lowercase
-    if include_numbers:
+    if dpg.get_value('include_numbers'):
         characters += string.digits
-    if include_symbols:
+    if dpg.get_value('include_symbols'):
         characters += string.punctuation
+    return characters
 
-    dpg.set_value('tmp_checkbox_info','')
-
+def generate_password(sender):
+    if sender != 'generate_button' and not dpg.get_value('auto_generate_toggle'):
+        return
+    
+    characters = get_characters()
     if not characters:
         dpg.set_value('tmp_checkbox_info',ERROR_NO_CHAR_SELECTED)
         dpg.set_value('password_field', '')
         return
+    dpg.set_value('tmp_checkbox_info','')
+    dpg.set_value('copy_status', '')
     
     length = dpg.get_value('password_length')
-    password = ''.join(random.choice(characters) for _ in range(length))
+    password = ''.join(secrets.choice(characters) for _ in range(length))
     dpg.set_value("password_field", password)
 
     if dpg.get_value('auto_copy_toggle'):
@@ -39,6 +36,9 @@ def generate_password(sender):
 
 def toggle_password_visibility():
     dpg.configure_item('password_field', password=not dpg.get_value('show_password'))
+
+def clear_status():
+    dpg.set_value('copy_status','')
 
 def copy_password():
     password = dpg.get_value('password_field')
@@ -48,9 +48,7 @@ def copy_password():
     dpg.set_clipboard_text(password)
     dpg.set_value('copy_status', INFO_PASSWORD_COPIED)
 
-def exit_app(sender):
-    print('Exit PyPass clicked')
-    print(f'Sender: {sender}')
+def exit_app():
     dpg.stop_dearpygui()
 
 def close_exit_dialog():
--- app/config.py
+++ app/config.py
@@ -11,4 +11,4 @@ ERROR_PASSWORD_EMPTY = 'Password field is empty!'
 INFO_PASSWORD_COPIED = "Password successfully copied!"
 
 #App Config
-APP_VERSION = '0.4.1'
+APP_VERSION = '0.4.2'
